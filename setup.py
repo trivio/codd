8f7f8d982bb19d1ab3b8953364a1db14d78be10a
Scott Robertson <srobertson@codeit.com>
2013-06-21 13:51:53 -0700

    version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except IOError:
 setup(
   name='codd',
   py_modules = ['codd'],
-  version='0.1.6',
+  version='0.1.7',
   description='relational alegrbra for functional programs',
   long_description=README,
   author='Scott Robertson',
